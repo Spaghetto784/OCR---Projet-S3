3f4481dfa07020acc8586e7c2708be09d5ad7d55
DekLoZz <pilou.games59@gmail.com>
2024-11-07 01:09:36 +0100

    NeuralChanges

--- src/main_neural_net.c
+++ src/main_neural_net.c
@@ -11,9 +11,9 @@ int main(int argc, char *argv[])
     // Train the network on the truth data
     for (int epoch = 0; epoch < 10000; epoch++)
     {
-        train(0, 0, 0);
+        train(0, 0, 1);
         train(0, 1, 0);
-        train(1, 0, 1);
+        train(1, 0, 0);
         train(1, 1, 1);
     }
 
